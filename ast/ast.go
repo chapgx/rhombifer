ba0d8c6d09bb884adf1f1a8deac960b94e9927e4
Richard Chapman Gomez <chapgx@codeh.io>
2026-04-18 13:47:07 -0400

    Removed all from the abstract syntax tree since I'm rethinking from the scratch for a more suitable shape for the CLI

--- ast/ast.go
+++ ast/ast.go
@@ -1,42 +1,77 @@
-// Abstract Syntax Tree
+// Package ast handlers the asbtrat syntax tree
 package ast
 
-import "github.com/racg0092/rhombifer/tokens"
+import (
+	"fmt"
 
-type Node interface {
+	"github.com/racg0092/rhombifer/tokens"
+)
+
+type Statement interface {
+	// TokenLiteral returns the literal token
 	TokenLiteral() string
-}
-
-type Cmd interface {
-	Node
-	commandNode()
-}
-
-type Flag interface {
-	Node
-	flagNode()
-}
-
-type Program struct {
-	Commands []Cmd
-}
-
-func (p Program) TokenLiteral() string {
-	//NOTE: it can probable be handly differently
-	if len(p.Commands) > 0 {
-		return p.Commands[0].TokenLiteral()
-	} else {
-		return ""
-	}
-}
-
-type Command struct {
-	Token tokens.Token
-	Name  string
-	// Sub   bool //NOTE: may not be needed
+	// TokenType returns the token type
+	TokenType() tokens.Type
 }
 
 type Identifier struct {
 	Token tokens.Token
 	Value string
+}
+
+func (i Identifier) TokenLiteral() string   { return i.Token.Literal }
+func (i Identifier) TokenType() tokens.Type { return i.Token.Type }
+
+type Command struct {
+	Token tokens.Token
+	Name  string
+	Value []Statement
+}
+
+func (c Command) TokenLiteral() string   { return c.Token.Literal }
+func (c Command) TokenType() tokens.Type { return c.Token.Type }
+
+type Flag struct {
+	Token tokens.Token
+	Name  string
+	Value []Statement
+}
+
+func (f Flag) TokenLiteral() string   { return f.Token.Literal }
+func (f Flag) TokenType() tokens.Type { return f.Token.Type }
+
+type ShortFLag struct {
+	Token tokens.Token
+	Name  string
+}
+
+func (sf ShortFLag) TokenLiteral() string   { return sf.Token.Literal }
+func (sf ShortFLag) TokenType() tokens.Type { return sf.Token.Type }
+
+type Program struct {
+	Tree []Statement
+}
+
+// PrintAST prints the AST into the standard output
+func PrintAST(ident int, nodes ...Statement) {
+	tab := ""
+	for i := 0; i < ident; i += 1 {
+		tab += "\t"
+	}
+
+	for _, node := range nodes {
+		switch t := node.(type) {
+		case Command:
+			fmt.Printf("%scommand => %s", tab, t.Name)
+			if len(t.Value) <= 0 {
+				break
+			}
+			ident := ident
+			ident += 1
+			PrintAST(ident, t.Value...)
+		case Flag:
+		case Identifier:
+		case ShortFLag:
+		}
+	}
 }
